aa44efb24f2d9bd38a3fe3e9fe69736b1717eb53
Michael <165153585+haystarkk@users.noreply.github.com>
2025-07-30 21:48:05 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,12 +1,15 @@
 -- task_2.sql
 
+-- Use the database created in Task 1
 USE alx_book_store;
 
+-- Create AUTHORS table
 CREATE TABLE AUTHORS (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
+-- Create BOOKS table
 CREATE TABLE BOOKS (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
@@ -16,6 +19,7 @@ CREATE TABLE BOOKS (
     FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
 );
 
+-- Create CUSTOMERS table
 CREATE TABLE CUSTOMERS (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
@@ -23,6 +27,7 @@ CREATE TABLE CUSTOMERS (
     address TEXT
 );
 
+-- Create ORDERS table
 CREATE TABLE ORDERS (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
@@ -30,6 +35,7 @@ CREATE TABLE ORDERS (
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
 );
 
+-- Create ORDER_DETAILS table
 CREATE TABLE ORDER_DETAILS (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
